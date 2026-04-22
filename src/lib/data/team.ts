d5df87432488097b051bd9474c43153a01b6f525
Joney Xiao <joney_xiao@epam.com>
2026-04-22 16:08:06 +0800

    refactor: use fastapi template

--- src/lib/data/team.ts
+++
@@ -1,6 +0,0 @@
-import type { TeamProfile } from '@/lib/types'
-import teamData from '@/data/fixtures/team.json'
-
-export async function getTeamProfile(): Promise<TeamProfile> {
-  return teamData as TeamProfile
-}
